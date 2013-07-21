34a2823f424e0da45233c2bf2e563ad2593bb4d9
Asbjorn Grandt <asbjorn@grandt.com>
2013-07-21 14:24:15 +0200

    Added proper html to teh test in index.php
    epub and mobi requires proper xhtml

--- index.php
+++ index.php
@@ -7,9 +7,26 @@
 		
 		$options = null;
 		
+		$content_start =
+			"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+			. "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\"\n"
+			. "    \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">\n"
+			. "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
+			. "<head>"
+			. "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n"
+			. "<title>Test Book</title>\n"
+			. "</head>\n"
+			. "<body>\n";
+
+		$content_end = "</body>\n</html>\n";
+	
 		$function = ucfirst($_GET['function']);
 		if (isset($_GET['str'])) {
-			$options = array("html" => "<h1>Hello, World</h1><p><strong>Here is some text, wrapped in 'p' and 'strong' HTML tags</strong></p><p><em>Here is some text, wrapped in 'p' and 'em' HTML tags</em></p>");
+			$options = array("html" => $content_start 
+				. "<h1>Hello, World</h1>\n"
+				. "<p><strong>Here is some text, wrapped in 'p' and 'strong' HTML tags</strong></p>\n"
+				. "<p><em>Here is some text, wrapped in 'p' and 'em' HTML tags</em></p>\n"
+				. $content_end);
 		}
 		$controllerName = "{$function}Controller";
 		$modelName = "{$function}Model";
